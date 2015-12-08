1b9ecd7c83fd3627f25ea2ace78af8653068f4a1
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-12-08 23:09:09 +0100

    Add base source files

---
+++ src/ofApp.cpp
@@ -0,0 +1,22 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+
+}
+
+void ofApp::update(){
+
+}
+
+void ofApp::draw(){
+
+}
+
+void ofApp::keyPressed(int key){
+
+}
+
+
+void ofApp::gotMessage(ofMessage msg){
+
+}
