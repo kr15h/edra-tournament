1041ed9f38a7d3b6a79338558ccf91ea678149e7
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2015-12-09 18:46:02 +0100

    Show countdown placeholder when hitting space

--- public/javascripts/brackets.js
+++ public/javascripts/brackets.js
@@ -1,5 +1,7 @@
 var interval;
 
+var keySpace = 32;
+
 function poll() {
   $.getJSON('http://localhost:3000/tournament', function(data) {
     $('.brackets').bracket({
@@ -15,6 +17,23 @@ function initPolling() {
   interval = setInterval(poll, 1000);
 }
 
+function toggleCountdown() {
+  if ($('.countdown').hasClass('hidden')) {
+    $('.countdown').removeClass('hidden');
+    $('body').addClass('noscroll');
+  } else {
+    $('.countdown').addClass('hidden');
+    $('body').removeClass('noscroll');
+  }
+}
+
 $(document).ready(function() {
   initPolling();
+});
+
+$(document).keypress(function(event) {
+  event.preventDefault();
+  if (event.keyCode === keySpace) {
+    toggleCountdown();
+  }
 });
